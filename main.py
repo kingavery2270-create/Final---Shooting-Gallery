b35d12087d2818cddf1af74a7085057e7bf9e131
Your Name <kingavery2270@gmail.com>
2026-05-19 16:25:46 -0500

    finished block falling, set up bombs but not detonation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import *
+import time, random
 
 ### CLASS and FUNCTION DEFINITIONS ###
 
@@ -16,13 +17,15 @@ def playing_area():
     pen.end_fill()
 
 class Player(Turtle):
-    def __init__(self,screen,color,rt_key,lt_key,f_key):
+    def __init__(self,x,y,screen,color,rt_key,lt_key,f_key):
         super().__init__()
         self.ht()
         self.speed(0)
         self.playercolor = color
         self.penup()
+        self.goto(x,y)
         self.color(color)
+        self.playercolor = color
         self.bullets = []
         self.setheading(90)
         self.shape('turtle')
@@ -41,11 +44,16 @@ class Player(Turtle):
         self.bullets.append(Bullet(self))
 
 class Block(Turtle):
-    def __init__(self,x,y):
+    def __init__(self,x,y,color,tipe):
         super().__init__()
         self.ht()
-        self.pu
-        self.color('gray')
+        self.seth(-90)
+        self.pu()
+        self.type = tipe
+        self.speed(0)
+        self.pencolor("black")
+        self.turtlesize(2,2,2)
+        self.color(color)
         self.shape('square')
         self.bullets = []
         self.health = 3
@@ -58,15 +66,18 @@ class Block(Turtle):
                 self.color('orange')
             elif self.health == 1:
                 self.color('red')
-        else:
+        elif self.health == 0 and self.type != 'bomb':
+            blocks.remove(self)
+            self.ht()
+        elif self.health == 2 and self.type == 'bomb':
             self.ht()
             blocks.remove(self)
+            
     def fall(self):
-        self.seth(-90)
-        self.fw(10)
+        self.forward(40)
 
 class Bullet(Turtle):
-    def __init__(self):
+    def __init__(self,player):
         super().__init__()
         self.ht()
         self.speed(0)
@@ -78,50 +89,85 @@ class Bullet(Turtle):
         self.st()
     
     
-    def move(self,player):
-        self.forward(15)
+    def move(self,turtle):
+        self.forward(5)
         
-        if self.ycor() > 235 or self.ycor() < -235:
+        if self.ycor() > 250 or self.ycor() < -250:
             self.speed(0)
             self.setheading(180 - turtle.heading())
             self.fd(10)
-            self.speed(1)
-            self.append(create_turtle())
+            self.speed(0)
+            
 
-        if self.xcor() > 235 or self.xcor() < -235:
+        if self.xcor() > 120 or self.xcor() < -120:
             self.speed(0)
             self.setheading(180 - turtle.heading())
             self.fd(10)
-            self.speed(1)
-            self.append(create_turtle())
+            self.speed(0)
+        
+    def hit(self,blocks,player):
+        for block in blocks:
+            if self.distance(block) <= 35:
+                player.bullets.remove(self)
+                self.ht()
+                block.health -= 1
+                block.damage()
+                
+
 
 
 def update():
-    for bullet in p2.bullets:
-        bullet.move()
-    for bullet in p1.bullets:
-        bullet.move()
-    for y in range(190,149,-20):
-        for x in range(-40,41,20):
-            blocks.append(Block(x,y))
+    global start, blocks
+    if time.time() - start > 2:
+        start = time.time()
+        screen.tracer(0)
+        for block in blocks:
+            block.fall()
+        for x in range(-80,101,40):
+            tipe = random.randint(1,15)
+            if tipe == 1:
+                blocks.append(Block(x,220,"green",'bomb'))
+            elif len(blocks)%3==0 and tipe != 1:
+                blocks.append(Block(x,220,"light gray",'normal'))
+            elif len(blocks)%3==1 and tipe != 1:
+                blocks.append(Block(x,220,"gray",'normal'))
+            else:
+                blocks.append(Block(x,220,"dark gray",'normal'))
+        screen.tracer(1)
 
-    screen.ontimer(update,120)
+    for bullet in p2.bullets:
+        bullet.move(p2)
+        bullet.hit(blocks,p2)
+    for bullet in p1.bullets:
+        bullet.move(p1)
+        bullet.hit(blocks,p1)
+    
+
+    screen.ontimer(update,80)
 ### PROGRAM ###
 screen = Screen()
 screen.bgcolor("black")
 screen.listen()
 screen.setup(520,520)
+start = time.time()
 blocks = []
-p1 = Player(screen,'red','d','a','f')
-p2 = Player(screen,'blue','l','j',';')
 playing_area()
-p1.ht()
-p1.goto(-45,-220)
-p1.st()
-p2.ht()
-p2.goto(45,-220)
-p2.st()
+p1 = Player(-45,-220,screen,'red','d','a','f')
+p2 = Player(45,-220,screen,'blue','l','j',';')
+
+
+
 
 screen.onkey(update,'space')
+screen.tracer(0)
+for y in range(140,225,40):
+    for x in range(-80,101,40):
+        if len(blocks)%3==0:
+            blocks.append(Block(x,y,"light gray",'normal'))
+        elif len(blocks)%3==1:
+            blocks.append(Block(x,y,"gray",'normal'))
+        else:
+            blocks.append(Block(x,y,"dark gray",'normal'))
+screen.tracer(1)
 
 screen.exitonclick()
