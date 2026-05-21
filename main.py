4b7ddaab1729176fe2a29e11bd713fd10971d2d4
Your Name <kingavery2270@gmail.com>
2026-05-21 16:32:59 -0500

    finished bombs ask next class

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class Player(Turtle):
         super().__init__()
         self.ht()
         self.speed(0)
+        self.score = 0
         self.playercolor = color
         self.penup()
         self.goto(x,y)
@@ -60,69 +61,120 @@ class Block(Turtle):
         self.goto(x,y)
         self.st()
 
-    def damage(self):
-        if self.health >= 0:
+    def damage(self,player):
+        if self.type == 'bomb':
+            self.ht()
+            blocks.remove(self)
+            player.score += 1
+            self.explode()
+        if self.health > 0:
             if self.health == 2:
                 self.color('orange')
             elif self.health == 1:
                 self.color('red')
         elif self.health == 0 and self.type != 'bomb':
-            blocks.remove(self)
-            self.ht()
-        elif self.health == 2 and self.type == 'bomb':
             self.ht()
             blocks.remove(self)
-            
+            player.score += 1
+        
+
     def fall(self):
         self.forward(40)
+
+    def explode(self):
+        # if time.time()- self.start >2:
+        tracer(0)
+        for block in blocks:
+            if self.distance(block) <= 80:
+                block.ht()
+                blocks.remove(block)
+        tracer(1)
 
 class Bullet(Turtle):
     def __init__(self,player):
         super().__init__()
         self.ht()
+        self.score = 0
         self.speed(0)
         self.color(player.playercolor)
         self.penup()
         self.goto(player.xcor(),player.ycor())
         self.setheading(player.heading())
         self.forward(20)
+        self.player =player
         self.st()
     
-    
-    def move(self,turtle):
-        self.forward(5)
+    def die(self):
+        self.ht()
+        if bullet in self.player.bullets:
+            self.player.bullets.remove(self)        
+
+    def move(self):
+        self.forward(15)
         
         if self.ycor() > 250 or self.ycor() < -250:
-            self.speed(0)
-            self.setheading(180 - turtle.heading())
-            self.fd(10)
-            self.speed(0)
+            self.die()
             
 
         if self.xcor() > 120 or self.xcor() < -120:
             self.speed(0)
-            self.setheading(180 - turtle.heading())
+            self.setheading(180 - self.heading())
             self.fd(10)
             self.speed(0)
         
-    def hit(self,blocks,player):
-        for block in blocks:
-            if self.distance(block) <= 35:
-                player.bullets.remove(self)
-                self.ht()
-                block.health -= 1
-                block.damage()
-                
 
+# def hit(self,player):
+#     for block in blocks:
+#         if block.distance(self) <= 35:
+#             self.ht()
+#             if bullet in player.bullets:
+#                 self.die()
+#             block.health -= 1
+#             block.damage(player)
+def setscoreboard():
+            yertle = Turtle()
+            yertle.ht()
+            yertle.speed(0)
+            yertle.begin_fill()
+            yertle.color('light gray')
+            yertle.penup()
+            yertle.goto(-110,-240)
+            yertle.pendown()
+            yertle.goto(110,-240)
+            yertle.goto(110,-210)
+            yertle.goto(-110,-210)
+            yertle.goto(-110,-240)
+            yertle.end_fill()
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.ht()
+        self.penup()
+        self.color('black')
+        self.speed(0)
+        self.begin_fill()
+            
+    def printscore(self,player,x):
+        self.goto(x,-240)
+        self.color('black')
+        self.pu()
+        self.pd()
+        self.write(player.score,font=['Arial',20,'normal'])
 
 
 def update():
-    global start, blocks
+    global start, blocks, bullet, block
     if time.time() - start > 2:
         start = time.time()
         screen.tracer(0)
         for block in blocks:
+            if block.ycor() < -240:
+                blocks.clear()
+                
+                
             block.fall()
+
         for x in range(-80,101,40):
             tipe = random.randint(1,15)
             if tipe == 1:
@@ -134,13 +186,31 @@ def update():
             else:
                 blocks.append(Block(x,220,"dark gray",'normal'))
         screen.tracer(1)
-
+    p1score.printscore(p1,-80)
+    p2score.printscore(p2,80)
     for bullet in p2.bullets:
-        bullet.move(p2)
-        bullet.hit(blocks,p2)
+        bullet.move()
+        for block in blocks:
+            p2currentscore = p2.score
+            if block.distance(bullet) <= 35:
+                bullet.die()
+                block.health -= 1
+                block.damage(p2)
+            if p2.score > p2currentscore:
+                p2score.clear()
+                p2score.printscore(p1,-80)
+        
     for bullet in p1.bullets:
-        bullet.move(p1)
-        bullet.hit(blocks,p1)
+        bullet.move()
+        for block in blocks:
+            p1currentscore = p1.score
+            if block.distance(bullet) <= 35:
+                bullet.die()
+                block.health -= 1
+                block.damage(p1)
+            if p1.score > p1currentscore:
+                p1score.clear()
+                p1score.printscore(p1,-80)
     
 
     screen.ontimer(update,80)
@@ -152,9 +222,10 @@ screen.setup(520,520)
 start = time.time()
 blocks = []
 playing_area()
-p1 = Player(-45,-220,screen,'red','d','a','f')
-p2 = Player(45,-220,screen,'blue','l','j',';')
-
+p1 = Player(-45,-180,screen,'red','d','a','f')
+p2 = Player(45,-180,screen,'blue','l','j',';')
+p1score = Score()
+p2score = Score()
 
 
 
@@ -162,12 +233,15 @@ screen.onkey(update,'space')
 screen.tracer(0)
 for y in range(140,225,40):
     for x in range(-80,101,40):
-        if len(blocks)%3==0:
-            blocks.append(Block(x,y,"light gray",'normal'))
-        elif len(blocks)%3==1:
-            blocks.append(Block(x,y,"gray",'normal'))
+        tipe = random.randint(1,15)
+        if tipe == 1:
+            blocks.append(Block(x,220,"green",'bomb'))
+        elif len(blocks)%3==0 and tipe != 1:
+            blocks.append(Block(x,220,"light gray",'normal'))
+        elif len(blocks)%3==1 and tipe != 1:
+            blocks.append(Block(x,220,"gray",'normal'))
         else:
-            blocks.append(Block(x,y,"dark gray",'normal'))
+            blocks.append(Block(x,220,"dark gray",'normal'))
 screen.tracer(1)
-
+setscoreboard()
 screen.exitonclick()
